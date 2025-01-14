6fadc9b798784500f522e9a6c018d057abee0954
marasulov7 <marasulovalish@gmail.com>
2025-01-14 11:07:20 +0500

    Добавление выборки столбца в список

--- csvAnalyst.py
+++ csvAnalyst.py
@@ -48,12 +48,23 @@ def pandas_read_csv(file_name):
     label_11['text'] = cnt_rows
     label_21['text'] = cnt_columns
     return df
-    
+
+# Выборка столбца в список
+def get_column(df, column_ix):
+    cnt_rows = df.shape[0]
+    lst = []
+    for i in range(cnt_rows):
+        lst.append(df.iat[i, column_ix])
+    return lst
+
 # Обработчик нажатия кнопки
 def process_button():
     file_name = do_dialog()
     label_01['text'] = file_name
-    pandas_read_csv(file_name)
+    df = pandas_read_csv(file_name)
+    lst = get_column(df, 0)
+    for item in lst:
+        output_text.insert(tk.END, str(item) + os.linesep)
     mb.showinfo(title = None, message="Готово")
     
 # Создание кнопки
