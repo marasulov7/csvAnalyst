56606362af2976d56b155758b68bfda7f2a5a1dd
marasulov7 <marasulovalish@gmail.com>
2025-01-14 10:30:55 +0500

    Добавление чтения текущей папки и вывода файла

--- csvAnalyst.py
+++ csvAnalyst.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter.scrolledtext import ScrolledText as st
 from tkinter import messagebox as mb
 from tkinter import filedialog as fd
+import os
 
 # Создание главного окна
 window=tk.Tk()
@@ -34,7 +35,8 @@ output_text.grid(row=3, column=1, padx=10, pady=10, sticky="w")
 
 # Диалог открытия файла
 def do_dialog():
-    name=fd.askopenfilename()
+    my_dir = os.getcwd()
+    name=fd.askopenfilename(initialdir=my_dir)
     return name
 
 # Обработчик нажатия кнопки
